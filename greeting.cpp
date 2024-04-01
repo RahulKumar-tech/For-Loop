f6a96006fd1808871c9badb0b3d300c57bdbc7de
RAHUL KUMAR <77164649+RahulKumar-tech@users.noreply.github.com>
2024-04-01 18:53:44 +0530

    Add files via upload

---
+++ greeting.cpp
@@ -0,0 +1,16 @@
+//For Loop
+//when in computers we wanna write some statements multiple times, 
+// or we wanna do some specific things multiple times,
+//or if any things that is connected to any pattern, then we use loops.
+#include<iostream>
+ using namespace std;
+int main(){
+    // 10 times Good Morning
+    // we do codings to skip repetition 😊😊
+    //automate , repetition
+    for(int i =1; i<=5; i++){
+         cout<<"Good Morning"<<endl;
+    }
+   
+}
+
